c774b62b2dfe8a7b6733f180b79831ed30737e07
Harshit Singh Rathore <harsh.rathore.2303@gmail.com>
2025-08-31 18:35:26 +0530

    database connection success

--- healthcarebackend/users/models.py
+++ healthcarebackend/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, unique=True)
+    password = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.name
