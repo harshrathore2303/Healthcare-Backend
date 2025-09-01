29bb9b999832b28f37e1ea890e862433a690fa1b
Harshit Singh Rathore <harsh.rathore.2303@gmail.com>
2025-09-01 20:50:08 +0530

    allowed host

--- healthcarebackend/healthcarebackend/settings.py
+++ healthcarebackend/healthcarebackend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "healthcare-backend-gefb.onrender.com"]
 
 
 # Application definition
